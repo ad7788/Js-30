3650c6364cf7884b2fe9195108f46db134f700e2
Aditya <aditya.a@altavistainfosolutions.com>
2024-07-13 21:06:02 +0530

    completed Day 1 challenge

---
+++ Day1/Day1.js
@@ -0,0 +1,35 @@
+//Task1
+var num = 10;
+console.log(num);
+
+//Task2
+let name = "Aditya";
+console.log(name);
+
+//Task3
+const bool = true;
+console.log(bool);
+
+//Task4
+let num1 = 10;
+let str = "Aditya";
+let bool1 = true;
+let obj = { name: "Aditya" };
+let arr = [1, 2, 3];
+console.log(typeof num1);
+console.log(typeof str);
+console.log(typeof bool1);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//Task5
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+
+//Task6
+const ab = 10;
+console.log(ab);
+ab = 20;
+console.log(ab); // will get error(TypeError: Assignment to constant variable)
